faefdb0393cfcd79108bd9909a21f6eeffc55415
Marco Costalba <mcostalba@gmail.com>
2013-05-27 17:42:52 +0200

    Shrink engine UCI name
    Some GUI have problems with long names.
    
    Reported by George Speight.
    
    No functional change.

--- src/misc.cpp
+++ src/misc.cpp
@@ -55,7 +55,7 @@ static const string Tag = "";
 const string engine_info(bool to_uci) {
 
   const string months("Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec");
-  const string cpu64(Is64Bit ? " 64bit" : "");
+  const string cpu64(Is64Bit ? " 64" : "");
 #if PA_GTB
   string popcnt(HasPopCnt ? " SSE4.2" : "");
 #else
@@ -72,7 +72,7 @@ const string engine_info(bool to_uci) {
       date >> month >> day >> year;
 
       s << Tag << string(Tag.empty() ? "" : " ") << setfill('0') << setw(2) << day
-        << "-" << setw(2) << (1 + months.find(month) / 4) << "-" << year.substr(2);
+        << setw(2) << (1 + months.find(month) / 4) << year.substr(2);
   }
 
   s << cpu64 << popcnt << (to_uci ? "\nid author ": " by ")
