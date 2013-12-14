598bb04de6645bfd798e0440879baebc4f99e602
Shivkumar Shivaji <sshivaji@gmail.com>
2013-12-14 12:55:04 -0800

    Modified pgn loading directory

--- web/handlers/page_handlers.py
+++ web/handlers/page_handlers.py
@@ -4,7 +4,7 @@ import tornado
 class PGNHandler(tornado.web.RequestHandler):
     def get(self):
         print "Downloading pgn.."
-        gfile = open(expanduser("~/git/Stockfish/src/game.pgn"), "r")
+        gfile = open("/home/miniand/git/Stockfish/src/game.pgn", "r")
         self.set_header('Content-Type', 'application/x-chess-pgn')
         self.set_header('Content-Disposition', 'attachment; filename=game.pgn')
 
