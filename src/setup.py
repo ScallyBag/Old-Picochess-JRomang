29b44eb043a59b176bbe0b69490b12b490c8636b
Jean-Francois Romang <jeanfrancois.romang@gmail.com>
2014-02-03 10:04:10 +0100

    Add tablebases support.

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ module1 = Extension('stockfish',
                     sources = ['benchmark.cpp', 'evaluate.cpp', 'movepick.cpp', 'search.cpp', 'ucioption.cpp',
                                'bitbase.cpp', 'main.cpp', 'notation.cpp', 'thread.cpp', 'bitboard.cpp', 'material.cpp',
                                'pawns.cpp', 'timeman.cpp', 'book.cpp', 'misc.cpp', 'position.cpp', 'tt.cpp', 'endgame.cpp',
-                               'movegen.cpp', 'pyfish.cpp', 'uci.cpp'],extra_compile_args=args)
+                               'movegen.cpp', 'pyfish.cpp', 'uci.cpp', 'tbprobe.cpp'],extra_compile_args=args)
 
 setup (name = 'stockfish',
        version = '1.0',
